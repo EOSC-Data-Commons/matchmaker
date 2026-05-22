f2ce38a62db8a527921bcc2f534ec9cd23a062ff
Ritwik Shanker <ritwikshanker@yahoo.in>
2026-05-22 14:19:33 +0200

    Add eslint-plugin-react-you-might-not-need-an-effect

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,31 @@
+// eslint.config.js
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactYouMightNotNeedAnEffect from "eslint-plugin-react-you-might-not-need-an-effect"
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
     {ignores: ['dist', 'build', '.react-router']},
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended, reactYouMightNotNeedAnEffect.configs.recommended],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            'react-you-might-not-need-an-effect': reactYouMightNotNeedAnEffect,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                {allowConstantExport: true},
+            ],
+        },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
 )
